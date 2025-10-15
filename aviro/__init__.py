7fd0df8321ddbc389bd42bc59c548de054995e2e
Aarush Kukreja <aarush.kukreja@gmail.com>
2025-10-15 11:38:59 -0700

    this might be it

--- aviro/__init__.py
+++ aviro/__init__.py
@@ -1,8 +1,8 @@
 """Aviro - A Python package for [describe your package's purpose here]."""
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __author__ = "Your Name"
 __email__ = "your.email@example.com"
 
 # Import main classes/functions here if needed
-# from .span import SomeClass
+from .span import AviroClient, observe
